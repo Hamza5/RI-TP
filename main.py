4c078a0a6e20743a60f4c71ad679ec1c53a3473c
Hamza Abbad <hamza.abbad@gmail.com>
2016-11-02 15:10:04 +0100

    Ajouter le code intial
    Les classes CACMDocument et CACMParser

---
+++ main.py
@@ -0,0 +1,65 @@
+import collections.abc
+import re
+
+
+class CACMDocument:
+    """
+    Represent a single CACM document with an ID, title and summary.
+    """
+
+    def __init__(self, num, title, summary):
+        self.I = num
+        self.T = title
+        self.W = summary
+
+    def __str__(self):
+        return str(self.I) + '/ ' + self.T + '\n' + self.W
+
+    def get_title(self):
+        return self.T
+
+    def get_document_number(self):
+        return self.I
+
+    def get_summary(self):
+        return self.W
+
+
+class CACMParser(collections.abc.Iterator):
+    """
+    Iterator which return a CACMdocument on each call.
+    """
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(filepath) as f:
+            file_content = f.read()
+        self.documents = re.split(r'\n\.I', file_content)[1:]
+
+    def __next__(self):
+        try:
+            doc = self.documents.pop(0)
+        except IndexError:
+            raise StopIteration
+        num_rest = doc.split('.T')
+        I = int(num_rest[0])
+        rest = num_rest[1].split('.B')
+        title_summary = rest[0].split('.W')
+        T = title_summary[0].strip(' \n').replace('\n', ' ')
+        if len(title_summary) == 2:
+            W = title_summary[1].strip(' \n').replace('\n', ' ')
+        else:
+            W = ''
+        return CACMDocument(I, T, W)
+
+    def __iter__(self):
+        return self
+
+
+if __name__ == '__main__':
+    import sys
+    cacm = CACMParser(sys.argv[1])
+    element = None
+    for element in cacm:
+        pass
+    print(element)
